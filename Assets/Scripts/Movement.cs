fbb4f242fe9773d3af37293c0a2aa2c98135c631
martinbarross <martin.barrosudc@gmail.com>
2025-02-26 19:53:47 +0100

    Updated: Movimiento y colisiones conseguidas

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,22 +4,45 @@ public class Movement : MonoBehaviour
 {
     public CharacterController controller;                          // Controlador de personaje
     public Transform cameraTransform;
-    public float speed = 1.0f;
+    public float speed = 5f;
     public float mouseSensitivity = 2f;
+    public float jumpHeight = 1.5f;
+
+    private float gravity = -9.81f;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;                   // Bloqueamos el cursor
+        Cursor.visible = false;                                     // Ocultamos el cursor
+    }
 
     void Update()
     {
         // Movimiento del personaje
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
-        Vector3 move = cameraTransform.right * moveX + cameraTransform.forward * moveZ;
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
         controller.Move(move * speed * Time.deltaTime);
 
         // Movimiento de la cámara
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        transform.Rotate(Vector3.up * mouseX);
 
-        cameraTransform.Rotate(vector3.up * mouseX);
-        cameraTransform.Rotate(vector3.left * mouseY);
+        // Aplicamos gravedad
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
